21e5ef89f33a02dc663ea07ef952e3a7771ac7fa
Dave Hurt <arthurdent510@gmail.com>
2017-08-23 16:34:51 -0700

    added range finders

--- src/org/usfirst/frc6413/SteamWorks/commands/DriveToPeg.java
+++ src/org/usfirst/frc6413/SteamWorks/commands/DriveToPeg.java
@@ -2,35 +2,77 @@ package org.usfirst.frc6413.SteamWorks.commands;
 
 import org.usfirst.frc6413.SteamWorks.Robot;
 
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveToPeg extends Command {
+	private PIDController pid;
 	
 	public DriveToPeg(){
+		double distance = .2;
 		
+		requires(Robot.driveBase);
+		double kP = -10;
+		double kI = 0;
+		double kD = 0;
+		
+		pid = new PIDController(kP, kI, kD, new PIDSource() {
+			PIDSourceType m_sourceType = PIDSourceType.kDisplacement;
+
+			@Override
+			public double pidGet() {
+				return Robot.driveBase.getDistanceAhead();
+			}
+
+			@Override
+			public void setPIDSourceType(PIDSourceType pidSource) {
+				m_sourceType = pidSource;
+			}
+
+			@Override
+			public PIDSourceType getPIDSourceType() {
+				return m_sourceType;
+			}
+		}, new PIDOutput() {
+			@Override
+			public void pidWrite(double d) {
+				Robot.driveBase.driveForward(d);
+				System.out.println(d);
+			}
+		});
+		pid.setAbsoluteTolerance(0.01);
+		pid.setSetpoint(distance);
+		pid.setOutputRange(0, .6);
 	}
 
 	// Called just before this Command runs the first time
     protected void initialize() {
-    	System.out.println("starting drive forward!");
-    	Robot.driveBase.driveForward(.5);
+    	pid.reset();
+		pid.enable();
+		System.out.println("Staring drive to peg");
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveBase.driveForward(.75);
+    	//Robot.driveBase.driveForward(.75);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	double distance = Robot.driveBase.getDistanceAhead();
-    	System.out.println(distance);
-        return distance < .48;
+    	//double distance = Robot.driveBase.getDistanceAhead();
+    	//System.out.println(distance);
+        //return distance < .48;
+    	return pid.onTarget();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	pid.disable();
     	Robot.driveBase.driveForward(0);
+    	System.out.println("done with drive to peg");
     }
 
     // Called when another command which requires one or more of the same
